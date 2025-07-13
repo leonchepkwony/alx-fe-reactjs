076a0bf5724a44154a5ce1de15a9140e001aeaac
Leonard Chepkwony <leonchepkwony@gmail.com>
2025-07-13 07:44:41 +0300

    Create a User Profile Card Using Props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,15 +6,17 @@ import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Header />
-      <MainContent />
-      <Footer />
+      {/* <Header />
+      <MainContent /> */}
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      {/* <Footer /> */}
     </>
   );
 }
