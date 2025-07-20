f1232c7917692f720ced457cc70692528257974a
Leonard Chepkwony <leonchepkwony@gmail.com>
2025-07-20 19:34:24 +0300

    updated UserProfile

---
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { UserContext } from "./UserContext";
+
+function UserProfile() {
+  const userData = useContext(UserContext);
+  return (
+    <div>
+      <h2>{userData.name}</h2>
+      <p>Age: {userData.age}</p>
+      <p>Bio: {userData.bio}</p>
+    </div>
+  );
+}
+
+export default UserProfile;
